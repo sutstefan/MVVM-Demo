d5e62816073ac303b1009d56c4becca3122d3e89
Stefan Sut <sutstefan@yahoo.com>
2017-07-11 13:06:54 +0300

    Podfile added; Main.storboard removed

--- MVVM-Demo/AppDelegate.swift
+++ MVVM-Demo/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController.init(rootViewController: mainViewController)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+        
         return true
     }
 
